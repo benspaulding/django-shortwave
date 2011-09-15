b37fd0cc2291dae9e301ccf12f8cd2a6e92eb0c1
Ben Spaulding <ben@benspaulding.us>
2011-09-15 15:55:40 -0500

    Reformat some setup arguments.

--- setup.py
+++ setup.py
@@ -15,16 +15,25 @@ setup(
     license='BSD',
     download_url='http://github.com/benspaulding/django-shortwave/tarball/v0.9.0',
     long_description = read('README.rst'),
-    packages = ['shortwave', 'shortwave.tests'],
-    package_data = {'shortwave': ['fixtures/*',
-                                  'locale/*/LC_MESSAGES/*',
-                                  'templates/shortwave/*']},
-    classifiers=['Development Status :: 4 - Beta',
-                 'Environment :: Web Environment',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Topic :: Internet :: WWW/HTTP :: Site Management'],
+    packages = [
+        'shortwave',
+        'shortwave.tests'
+    ],
+    package_data = {
+        'shortwave': [
+            'fixtures/*',
+            'locale/*/LC_MESSAGES/*',
+            'templates/shortwave/*',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
 )
