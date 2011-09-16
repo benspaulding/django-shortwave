6764d43e380de2183b7fa33cbe57b6efd7cdfa16
Ben Spaulding <ben@benspaulding.us>
2011-09-15 19:23:46 -0500

    Add to CommandTriggerField docstring.

--- shortwave/forms.py
+++ shortwave/forms.py
@@ -4,12 +4,16 @@ from django.forms.fields import RegexField
 from django.utils.translation import ugettext_lazy as _
 
 
-# Match any number of alpha-numeric characters or a single * character.
+# Match any number of alpha-numeric characters or a single `*` character.
 trigger_re = re.compile(r'^(?:(?:\w)+|(?:\*)?)$')
 
-
 class CommandTriggerField(RegexField):
-    """Form field for a Shortwave command trigger."""
+    """
+    Form field for a Shortwave command trigger.
+
+    Valid values are any number of alpha-numeric characters or `*`.
+
+    """
 
     default_error_messages = {
         'invalid': _(u'Enter a valid trigger.'),
