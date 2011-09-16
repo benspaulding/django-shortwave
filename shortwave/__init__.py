2b60e939fb8d4405887266baacd2cd1e70130d2e
Ben Spaulding <ben@benspaulding.us>
2011-09-15 23:41:56 -0500

    Bump version number.
    Docs and an actual release on PyPI call for a bump.

--- shortwave/__init__.py
+++ shortwave/__init__.py
@@ -26,7 +26,7 @@ Refer to the documentation_ for further information.
 from django.utils.translation import ugettext_lazy as _
 
 
-VERSION = '0.9.0'
+VERSION = '0.9.1'
 
 # Mark the app_label for translation.
 _(u'shortwave')
